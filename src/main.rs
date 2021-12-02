9782e714a41de6fbac284adf8ed1186ac221827d
ggichuru <ggichuru.dev@gmail.com>
2021-12-03 02:46:52 +0300

    Init project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+pub struct App {
+    pub blocks: Vec,
+}
+
+#[derive(Serialize. Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64
+}
+
+
+fn main() {
+    
+}
